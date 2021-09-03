9414bc6629a74b67a17b4cb8490be99c6818f464
Violetta Petrova <vwondercame@gmail.com>
2021-09-02 22:19:51 -0400

    create input fields for mainpage"

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import './App.css';
+import { Component } from 'react';
+import './App.scss';
+import Form from './Form/Form';
 
 function App() {
   return (
+    <main>
    <header>
      <h1>Love Station</h1>
    </header>
+   <Form />
+  </main>
   );
 }
 
---
+++ src/Form/Form.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function Form() {
+    return (
+        <section className="lovematch">
+            <form className="lovematch-form">
+            <div className="lovematch-form__container">
+                <label className="lovematch-form__title"> Lover 1</label>
+                <input className="lovematch-form__name-input lovematch-form__name-input1" placeholder="first lover's name"/>
+            </div>
+
+            <div className="lovematch-form__container">
+                <label className="lovematch-form__title"> Lover 1</label>
+                <input className="lovematch-form__name-input lovematch-form__name-input1" placeholder="first lover's name"/>
+            </div>
+
+            </form>
+
+        </section>
+    )
+}
+
+export default Form
