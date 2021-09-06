8ae8a74947fd011bfd0a0d6f2964a7a352f8c807
Violetta Petrova <vwondercame@gmail.com>
2021-09-05 21:48:03 -0400

    blah blah

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -1,13 +1,60 @@
-import React from "react";
+import React, {Component} from "react";
+import axios from "axios";
+// import { createRef } from "react";
 
-function Form() {
+class Form extends Component {
+  state = {
+    options: {
+      method: 'GET',
+      url: 'https://love-calculator.p.rapidapi.com/getPercentage',
+      params: {fname: 'John', sname: 'Alice'},
+      headers: {
+        'x-rapidapi-host': 'love-calculator.p.rapidapi.com',
+        'x-rapidapi-key': '6d23cc90f1mshbaa369c944f52d7p1541e8jsn0f06105612ee'
+      }
+    }
+  };
+  
+    handleSubmit = (event) => {
+      event.preventDefault();
+      const fname = event.target.fname.value;
+      const sname = event.target.sname.value;
+
+      this.setState({
+        options: {
+          method: 'GET',
+          url: 'https://love-calculator.p.rapidapi.com/getPercentage',
+          params: {fname: fname, sname: sname },
+          headers: {
+            'x-rapidapi-host': 'love-calculator.p.rapidapi.com',
+            'x-rapidapi-key': '6d23cc90f1mshbaa369c944f52d7p1541e8jsn0f06105612ee'
+          }
+        }
+      }, () => {
+        axios.get(this.state.options)
+        .then((response) => {
+        console.log(response.data);
+        this.setState({
+          ...this.state,
+          percentage: response.data.percentage,
+          result: response.data.result
+        })
+      
+      }).catch(function (error) {
+        console.error(error);
+      });
+      })      
+    }
+  
+
+render() {
   return (
     <section className="lovematch">
-      <form className="lovematch-form">
+      <form className="lovematch-form" onSubmit={this.handleSubmit} >
         <div className="lovematch-form__input-container">
           <div className="lovematch-form__container">
             <label className="lovematch-form__title"> Lover 1</label>
-            <input
+            <input name="fname"
               className="lovematch-form__name-input lovematch-form__name-input1"
               placeholder="first lover's name"
             />
@@ -15,7 +62,7 @@ function Form() {
 
           <div className="lovematch-form__container">
             <label className="lovematch-form__title"> Lover 2</label>
-            <input
+            <input name="sname"
               className="lovematch-form__name-input lovematch-form__name-input2"
               placeholder="second lover's name"
             />
@@ -23,7 +70,7 @@ function Form() {
         </div>
 
         <div className="lovematch-form__button-container">
-          <button
+          <button 
             id="lovermatch_button"
             className="comment-section__button"
             type="submit"
@@ -34,8 +81,8 @@ function Form() {
       </form>
 
       <div className="lovematch__result">
-        <h2>85%</h2>
-        <h3>Great Match</h3>
+        <h2>{this.state.percentage}</h2>
+        <h3>{this.state.result}</h3>
       </div>
 
 
@@ -44,6 +91,7 @@ function Form() {
 
 
   );
+}
 }
 
 export default Form;
